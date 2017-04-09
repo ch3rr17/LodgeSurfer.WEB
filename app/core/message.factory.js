fec830c3983dd3b6dfc41417ccec1499f4678e96
Edison Ngo <edisonngo@gmail.com>
2017-04-08 18:09:51 -0700

    add conversation

---
+++ app/core/message.factory.js
@@ -0,0 +1,64 @@
+(function() {
+    'use strict';
+
+   angular
+        .module('app')
+        .factory('MessageFactory', MessageFactory);
+
+   MessageFactory.$inject = ['$http', '$q', 'apiUrl'];
+
+   /* @ngInject */
+    function MessageFactory($http, $q, apiUrl) {
+        var service = {
+          newConvo: newConvo,
+          newMessage: newMessage
+        };
+
+       return service;
+
+       function newConvo(convo) {
+          var defer = $q.defer();
+          $http({
+            method: 'POST',
+            url: apiUrl + 'conversations',
+            data: convo
+          })
+          .then(
+            function(response) {
+              defer.resolve(response);
+              console.log(response);
+            },
+            function(error) {
+              defer.reject(error);
+            }
+          );
+          return defer.promise;
+
+       }
+
+       function newMessage(userId, conversationId, message) {
+          var defer = $q.defer();
+          $http({
+            method: 'POST',
+            url: apiUrl + 'messages/',
+            params: {
+              userId: message.userId,
+              conversationId: message.conversationId
+
+            },
+            data: message
+          })
+          .then(
+            function(response) {
+              defer.resolve(response);
+              console.log(response);
+            },
+            function(error) {
+              defer.reject(error);
+            }
+          );
+          return defer.promise;
+
+       }
+    }
+})();
--- app/message/message.controller.js
+++ app/message/message.controller.js
@@ -3,32 +3,46 @@
 
     angular
         .module('app')
-        .controller('messageController', messageController);
+        .controller('MessageController', MessageController);
 
-    messageController.$inject = ['MessageFactory', 'SweetAlert'];
+    MessageController.$inject = ['MessageFactory', 'SweetAlert', 'localStorageService', 'UserFactory'];
 
     /* @ngInject */
-    function messageController(MessageFactory, SweetAlert) {
+    function MessageController(MessageFactory, SweetAlert, localStorageService, UserFactory) {
         var vm = this;
+        vm.message = {};
 
-        vm.addConvo = function(userIdOne, userIdTwo) {
-            MessageFactory.newMessage(message)
+        vm.addConvo = function() {
+          var convo = {
+            userOneId: 1,
+            userTwoId: 2
+//            userOneId: localStorageService.get('localUserId'),
+//            userTwoId: vm.listing.userId
+          }
+            MessageFactory.newConvo(convo)
                 .then(
                     function(response) {
                         console.log(response.data);
-                        SweetAlert.swal("Your message has been sent");
+                        console.log(vm.message);
+                        console.log(response.data.conversationId);
+                        vm.addMessage(response.data.conversationId, vm.message);
                     },
-                    function(error) {
+                    function (error) {
                         console.log(error);
+                        console.log(vm.message);
                     }
                 );
         }
 
-        vm.addMessage = function(message) {
-            MessageFactory.newMessage(message)
+        vm.addMessage = function(userId, conversationId) {
+          var userId = 1;
+          console.log(userId);
+
+            MessageFactory.newMessage(userId, conversationId, vm.message)
                 .then(
                     function(response) {
                         console.log(response.data);
+                        vm.message.push(response.data);
                         SweetAlert.swal("Your message has been sent");
                     },
                     function(error) {
